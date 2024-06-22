22ce9bfa4afd157cf60e4459c007508f3d7823e9
Joe Caulfield <joe.caulfield@anza.xyz>
2024-06-21 19:57:51 -0500

    enable logging

--- harness/src/lib.rs
+++ harness/src/lib.rs
@@ -70,6 +70,12 @@ pub struct Mollusk {
 
 impl Default for Mollusk {
     fn default() -> Self {
+        #[rustfmt::skip]
+        solana_logger::setup_with_default(
+            "solana_rbpf::vm=debug,\
+             solana_runtime::message_processor=debug,\
+             solana_runtime::system_instruction_processor=trace",
+        );
         Self {
             compute_budget: ComputeBudget::default(),
             feature_set: FeatureSet::all_enabled(),
