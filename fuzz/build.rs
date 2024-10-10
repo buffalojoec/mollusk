3e48dd6835e8cfbe1b2d99b8b3b1d3397477b43c
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-10 08:14:01 +0800

    add protos

---
+++ fuzz/build.rs
@@ -0,0 +1,19 @@
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let proto_base_path = std::path::PathBuf::from("proto");
+
+    let protos = &[
+        proto_base_path.join("compute_budget.proto"),
+        proto_base_path.join("sysvars.proto"),
+        proto_base_path.join("invoke.proto"),
+    ];
+
+    protos
+        .iter()
+        .for_each(|proto| println!("cargo:rerun-if-changed={}", proto.display()));
+
+    prost_build::compile_protos(protos, &[proto_base_path])?;
+
+    Ok(())
+}
