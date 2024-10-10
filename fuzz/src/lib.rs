8d8103e12ab6f7a71260979f3b887066b731bf36
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-10 08:11:37 +0800

    init fuzz harness crate

---
+++ fuzz/src/lib.rs
@@ -0,0 +1,6 @@
+//! Mollusk SVM Fuzz: Fuzzing harness for SVM programs.
+//!
+//! Note, although the fuzz harness provides an easy way to fuzz programs with
+//! Mollusk, it is not required. Developers can use this fuzz harness on their
+//! own custom SVM entrypoint. Hence the distinction between fixture types and
+//! Mollusk types.
