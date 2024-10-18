774c55bafc3b0e58647ad7410c7c6d31d9039368
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-18 17:09:20 +0700

    fuzz: fixture i/o

--- fuzz/fixture/build.rs
+++ fuzz/fixture/build.rs
@@ -13,7 +13,9 @@ fn main() -> Result<()> {
         .iter()
         .for_each(|proto| println!("cargo:rerun-if-changed={}", proto.display()));
 
-    prost_build::compile_protos(protos, &[proto_base_path])?;
+    prost_build::Config::new()
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .compile_protos(protos, &[proto_base_path])?;
 
     Ok(())
 }
--- fuzz/fixture/src/lib.rs
+++ fuzz/fixture/src/lib.rs
@@ -15,7 +15,15 @@ pub mod proto {
 }
 pub mod sysvars;
 
-use crate::{context::Context, effects::Effects, proto::InstrFixture as ProtoFixture};
+use {
+    crate::{context::Context, effects::Effects, proto::InstrFixture as ProtoFixture},
+    prost::Message,
+    std::{
+        fs::{self, File},
+        io::{Read, Write},
+        path::Path,
+    },
+};
 
 /// A fixture for invoking a single instruction against a simulated SVM
 /// program runtime environment, for a given program.
@@ -24,6 +32,73 @@ pub struct Fixture {
     pub input: Context,
     /// The fixture outputs.
     pub output: Effects,
+}
+
+impl Fixture {
+    /// Decode a `Protobuf` blob into a `Fixture`.
+    pub fn decode(blob: &[u8]) -> Self {
+        ProtoFixture::decode(blob)
+            .map(Into::into)
+            .unwrap_or_else(|err| panic!("Failed to decode fixture: {}", err))
+    }
+
+    /// Encode the `Fixture` into a `Protobuf` blob.
+    pub fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::new();
+        ProtoFixture::from(self)
+            .encode(&mut buf)
+            .expect("Failed to encode fixture");
+        buf
+    }
+
+    /// Dumps the `Fixture` to a protobuf binary blob file.
+    /// The file name is a hash of the fixture with the `.fix` extension.
+    pub fn dump_to_blob_file(self, dir: &str) {
+        let blob = self.encode();
+
+        let hash = solana_sdk::hash::hash(&blob);
+        let file_name = format!("instr-{}.fix", bs58::encode(hash).into_string());
+
+        write_file(Path::new(dir), &file_name, &blob);
+    }
+
+    /// Dumps the `Fixture` to a JSON file.
+    /// The file name is a hash of the fixture with the `.json` extension.
+    pub fn dump_to_json_file(self, dir_path: &str) {
+        let json = serde_json::to_string_pretty(&ProtoFixture::from(self))
+            .expect("Failed to serialize fixture to JSON");
+
+        let hash = solana_sdk::hash::hash(json.as_bytes());
+        let file_name = format!("instr-{}.json", bs58::encode(hash).into_string());
+
+        write_file(Path::new(dir_path), &file_name, json.as_bytes());
+    }
+
+    /// Loads a `Fixture` from a protobuf binary blob file.
+    pub fn load_from_blob_file(file_path: &str) -> Self {
+        if !file_path.ends_with(".fix") {
+            panic!("Invalid fixture file extension: {}", file_path);
+        }
+        let mut file = File::open(file_path).expect("Failed to open fixture file");
+        let mut blob = Vec::new();
+        file.read_to_end(&mut blob)
+            .expect("Failed to read fixture file");
+        Self::decode(&blob)
+    }
+
+    /// Loads a `Fixture` from a JSON file.
+    pub fn load_from_json_file(file_path: &str) -> Self {
+        if !file_path.ends_with(".json") {
+            panic!("Invalid fixture file extension: {}", file_path);
+        }
+        let mut file = File::open(file_path).expect("Failed to open fixture file");
+        let mut json = String::new();
+        file.read_to_string(&mut json)
+            .expect("Failed to read fixture file");
+        let fixture: ProtoFixture =
+            serde_json::from_str(&json).expect("Failed to deserialize fixture from JSON");
+        fixture.into()
+    }
 }
 
 impl From<ProtoFixture> for Fixture {
@@ -45,11 +120,10 @@ impl From<Fixture> for ProtoFixture {
     }
 }
 
-impl Fixture {
-    /// Decode a `Protobuf` blob into a `Fixture`.
-    pub fn decode(blob: &[u8]) -> Self {
-        <proto::InstrFixture as prost::Message>::decode(blob)
-            .map(Into::into)
-            .unwrap_or_else(|err| panic!("Failed to decode fixture: {}", err))
-    }
+fn write_file(dir: &Path, file_name: &str, data: &[u8]) {
+    fs::create_dir_all(dir).expect("Failed to create directory");
+    let file_path = dir.join(file_name);
+    let mut file = File::create(file_path).unwrap();
+    file.write_all(data)
+        .expect("Failed to write fixture to file");
 }
