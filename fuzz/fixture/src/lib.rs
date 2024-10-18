dde0ee3e24b60147de78ec948b75e7a71140cb22
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-18 14:23:35 +0700

    fuzz: init fuzz fixture library

---
+++ fuzz/fixture/src/lib.rs
@@ -0,0 +1,6 @@
+//! Mollusk SVM Fuzz: Mollusk-compatible fuzz fixture for SVM programs.
+//!
+//! Note: The fixtures defined in this library are compatible with Mollusk,
+//! which means developers can fuzz programs using the Mollusk harness.
+//! However, these fixtures (and this library) do not depend on the harness.
+//! They can be used to fuzz a custom entrypoint of the developer's choice.
