777b7dc1a90262fe88e8375ce4ea333e23fd07bb
Joe Caulfield <joe.caulfield@anza.xyz>
2024-10-19 11:51:41 +0700

    fix: consistent fixture file naming

--- harness/tests/dump_fixture.rs
+++ harness/tests/dump_fixture.rs
@@ -3,10 +3,7 @@
 use {
     mollusk_svm::{result::Check, Mollusk},
     mollusk_svm_fuzz_fixture::Fixture,
-    solana_sdk::{
-        account::AccountSharedData, instruction::Instruction, pubkey::Pubkey, system_instruction,
-        system_program,
-    },
+    solana_sdk::{account::AccountSharedData, pubkey::Pubkey, system_instruction, system_program},
     std::path::Path,
 };
 
@@ -62,7 +59,12 @@ fn clear() {
     }
 }
 
-fn mollusk_test() -> (Mollusk, Instruction, [(Pubkey, AccountSharedData); 2]) {
+#[test]
+fn test_dump() {
+    clear();
+    std::env::set_var("EJECT_FUZZ_FIXTURES", EJECT_FUZZ_FIXTURES);
+    std::env::set_var("EJECT_FUZZ_FIXTURES_JSON", EJECT_FUZZ_FIXTURES);
+
     let sender = Pubkey::new_unique();
     let recipient = Pubkey::new_unique();
 
@@ -72,7 +74,7 @@ fn mollusk_test() -> (Mollusk, Instruction, [(Pubkey, AccountSharedData); 2]) {
     let mollusk = Mollusk::default();
 
     let instruction = system_instruction::transfer(&sender, &recipient, transfer_amount);
-    let accounts = [
+    let accounts = vec![
         (
             sender,
             AccountSharedData::new(base_lamports, 0, &system_program::id()),
@@ -94,54 +96,51 @@ fn mollusk_test() -> (Mollusk, Instruction, [(Pubkey, AccountSharedData); 2]) {
 
     mollusk.process_and_validate_instruction(&instruction, &accounts, &checks);
 
-    (mollusk, instruction, accounts)
-}
+    // Validate the protobuf fixture matches the test environment.
+    let blob_fixture_path = find_fixture(&FileType::Blob).unwrap();
+    let blob_fixture = Fixture::load_from_blob_file(&blob_fixture_path);
+    assert_eq!(blob_fixture.input.compute_budget, mollusk.compute_budget);
+    assert_eq!(blob_fixture.input.feature_set, mollusk.feature_set);
+    assert_eq!(blob_fixture.input.sysvars.clock, mollusk.sysvars.clock);
+    assert_eq!(blob_fixture.input.sysvars.rent, mollusk.sysvars.rent);
+    assert_eq!(blob_fixture.input.program_id, instruction.program_id);
+    assert_eq!(
+        blob_fixture.input.instruction_accounts,
+        instruction.accounts
+    );
+    assert_eq!(blob_fixture.input.instruction_data, instruction.data);
+    assert_eq!(blob_fixture.input.accounts, accounts);
 
-#[test]
-fn test_dump() {
-    clear();
+    // Validate the JSON fixture matches the test environment.
+    let json_fixture_path = find_fixture(&FileType::Json).unwrap();
+    let json_fixture = Fixture::load_from_json_file(&json_fixture_path);
+    assert_eq!(json_fixture.input.compute_budget, mollusk.compute_budget);
+    assert_eq!(json_fixture.input.feature_set, mollusk.feature_set);
+    assert_eq!(json_fixture.input.sysvars.clock, mollusk.sysvars.clock);
+    assert_eq!(json_fixture.input.sysvars.rent, mollusk.sysvars.rent);
+    assert_eq!(json_fixture.input.program_id, instruction.program_id);
+    assert_eq!(
+        json_fixture.input.instruction_accounts,
+        instruction.accounts
+    );
+    assert_eq!(json_fixture.input.instruction_data, instruction.data);
+    assert_eq!(json_fixture.input.accounts, accounts);
 
-    // First try protobuf.
-    {
-        std::env::set_var("EJECT_FUZZ_FIXTURES", EJECT_FUZZ_FIXTURES);
+    // Ensure both files have the same name.
+    assert_eq!(
+        Path::new(&blob_fixture_path)
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .unwrap(),
+        Path::new(&json_fixture_path)
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .unwrap()
+    );
 
-        let (mollusk, instruction, accounts) = mollusk_test();
-
-        let fixture_path = find_fixture(&FileType::Blob).unwrap();
-        let fixture = Fixture::load_from_blob_file(&fixture_path);
-
-        assert_eq!(fixture.input.compute_budget, mollusk.compute_budget);
-        assert_eq!(fixture.input.feature_set, mollusk.feature_set);
-        assert_eq!(fixture.input.sysvars.clock, mollusk.sysvars.clock);
-        assert_eq!(fixture.input.sysvars.rent, mollusk.sysvars.rent);
-        assert_eq!(fixture.input.program_id, instruction.program_id);
-        assert_eq!(fixture.input.instruction_accounts, instruction.accounts);
-        assert_eq!(fixture.input.instruction_data, instruction.data);
-        assert_eq!(fixture.input.accounts, accounts);
-
-        std::env::remove_var("EJECT_FUZZ_FIXTURES");
-    }
-
-    // Now try JSON.
-    {
-        std::env::set_var("EJECT_FUZZ_FIXTURES_JSON", EJECT_FUZZ_FIXTURES);
-
-        let (mollusk, instruction, accounts) = mollusk_test();
-
-        let fixture_path = find_fixture(&FileType::Json).unwrap();
-        let fixture = Fixture::load_from_json_file(&fixture_path);
-
-        assert_eq!(fixture.input.compute_budget, mollusk.compute_budget);
-        assert_eq!(fixture.input.feature_set, mollusk.feature_set);
-        assert_eq!(fixture.input.sysvars.clock, mollusk.sysvars.clock);
-        assert_eq!(fixture.input.sysvars.rent, mollusk.sysvars.rent);
-        assert_eq!(fixture.input.program_id, instruction.program_id);
-        assert_eq!(fixture.input.instruction_accounts, instruction.accounts);
-        assert_eq!(fixture.input.instruction_data, instruction.data);
-        assert_eq!(fixture.input.accounts, accounts);
-
-        std::env::remove_var("EJECT_FUZZ_FIXTURES_JSON");
-    }
-
+    std::env::remove_var("EJECT_FUZZ_FIXTURES");
+    std::env::remove_var("EJECT_FUZZ_FIXTURES_JSON");
     clear();
 }
