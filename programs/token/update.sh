8ba756f4cbd6edd4ef5997969b9a10ffaf60b73d
Joe Caulfield <joe.caulfield@anza.xyz>
2024-11-26 15:03:51 +0900

    fix update scripts

--- programs/token/update.sh
+++ programs/token/update.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 solana program dump TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA ./src/elf/token.so -u mainnet-beta
 solana program dump TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb ./src/elf/token_2022.so -u mainnet-beta
 solana program dump ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL ./src/elf/associated_token.so -u mainnet-beta
-solana slot -u mainnet-beta | xargs -I {} sed -i '' 's|// Last updated at mainnet-beta slot height: .*|// Last updated at mainnet-beta slot height: {}|' ./src/lib.rs
+solana slot -u mainnet-beta | xargs -I {} sed -i '' 's|//! Last updated at mainnet-beta slot height: .*|//! Last updated at mainnet-beta slot height: {}|' ./src/lib.rs
