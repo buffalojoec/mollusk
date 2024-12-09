21c7cc5fd41b04c0eca9f5a617cf7a5ab39f5250
Joe Caulfield <jcaulfield135@gmail.com>
2024-12-09 06:56:59 +0000

    harness: rework sysvar caching

--- harness/src/lib.rs
+++ harness/src/lib.rs
@@ -44,10 +44,7 @@ use {
     accounts::CompiledAccounts,
     mollusk_svm_error::error::{MolluskError, MolluskPanic},
     solana_compute_budget::compute_budget::ComputeBudget,
-    solana_program_runtime::{
-        invoke_context::{EnvironmentConfig, InvokeContext},
-        sysvar_cache::SysvarCache,
-    },
+    solana_program_runtime::invoke_context::{EnvironmentConfig, InvokeContext},
     solana_sdk::{
         account::AccountSharedData, bpf_loader_upgradeable, feature_set::FeatureSet,
         fee::FeeStructure, hash::Hash, instruction::Instruction, pubkey::Pubkey,
@@ -175,17 +172,18 @@ impl Mollusk {
         );
 
         let invoke_result = {
-            let mut cache = self.program_cache.cache().write().unwrap();
+            let mut program_cache = self.program_cache.cache().write().unwrap();
+            let sysvar_cache = self.sysvars.setup_sysvar_cache(accounts);
             InvokeContext::new(
                 &mut transaction_context,
-                &mut cache,
+                &mut program_cache,
                 EnvironmentConfig::new(
                     Hash::default(),
                     None,
                     None,
                     Arc::new(self.feature_set.clone()),
                     self.fee_structure.lamports_per_signature,
-                    &SysvarCache::from(&self.sysvars),
+                    &sysvar_cache,
                 ),
                 None,
                 self.compute_budget,
