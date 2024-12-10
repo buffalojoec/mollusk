b729810e8853328692a176d94d8ce6f1425bac3d
Joe Caulfield <jcaulfield135@gmail.com>
2024-12-10 12:23:08 +0900

    update prepublish script

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -4,6 +4,6 @@ agave-install init 2.1.0
 rm -rf target
 cargo build
 ./scripts/build-test-programs.sh
-cargo +nightly fmt --all -- --check
-cargo +nightly clippy --all --all-features -- -D warnings
+cargo +nightly-2024-05-02 fmt --all -- --check
+cargo +nightly-2024-05-02 clippy --all --all-features -- -D warnings
 cargo test --all-features
