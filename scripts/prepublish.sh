5b62988c0980191787a107910bb870a747dfaf05
Joe Caulfield <jcaulfield135@gmail.com>
2025-01-21 14:34:49 +0000

    sf-agave: configure local manifest

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-agave-install init 2.1.0
+agave-install init edge
 rm -rf target
 cargo build
 ./scripts/build-test-programs.sh
-cargo +nightly-2024-05-02 fmt --all -- --check
-cargo +nightly-2024-05-02 clippy --all --all-features -- -D warnings
+cargo +nightly-2024-08-08 fmt --all -- --check
+cargo +nightly-2024-08-08 clippy --all --all-features -- -D warnings
 cargo test --all-features
